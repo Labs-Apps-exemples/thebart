27d35058045e17153ce1f3e97accf8ab7487c70a
Nelson Reitz <nailsonne@gmail.com>
2015-03-14 19:51:34 +0100

    Cache fill script

--- html/db_fill.php
+++ html/db_fill.php
@@ -1,10 +1,6 @@
 <?php
 
     /**
-     * model.php
-     */
-
-    /**
      * Executes SQL statement, possibly with parameters, returning
      * an array of all rows in result set or false on (non-fatal) error.
      */
@@ -64,37 +60,43 @@
         }
     }
 
-    $query = query("SELECT * FROM routes WHERE number = ?", 1);
+    // load stations xml
+    $xml = simplexml_load_file('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V');
 
-    foreach ($query[0] as $key => $value)
+    $stations = $xml->stations->station;
+
+    // insert every stations
+    foreach ($stations as $station)
     {
-        if ($key === 'config')
-        {
-            // split stations configuration
-            $config = explode(',', $value);
-
-            foreach ($config as $station_abbr)
-            {
-                // query current station
-                $query = query("SELECT * FROM stations WHERE abbr = ?", $station_abbr);
-
-                $station = [];
-                foreach($query[0] as $key => $value)
-                {
-                    $station[$key] = $value;
-                }
-
-                $route['config'][] = $station;
-            }
-        }
-        else
-        {
-            $route[$key] = $value;
-        }
+        query("INSERT INTO stations (abbr, name, latitude, longitude) VALUES (?, ?, ?, ?)",
+            $station->abbr, $station->name, $station->gtfs_latitude, $station->gtfs_longitude);
     }
-    // set MIME type
-    header('Content-type: application/json');
 
-    // output JSON
-    print(json_encode($route));
+    // load routes
+    $xml = simplexml_load_file('http://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V');
+
+    $routes = $xml->routes->route;
+
+    foreach ($routes as $route)
+    {
+
+        // load routeinfo xml
+        $routeinfo = simplexml_load_file('http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=' . $route->number .
+            '&key=MW9S-E7SL-26DU-VV8V');
+
+        // find route's configuration
+        $stations = $routeinfo->routes->route->config->station;
+
+        // prepare array
+        $config = [];
+        foreach ($stations as $station)
+        {
+            $config[] = $station;
+        }
+
+        // insert route
+        query("INSERT INTO routes (number, name, config) VALUES (?, ?, ?)",
+            $route->number, $route->name, implode(',', $config));
+    }
+
 ?>
