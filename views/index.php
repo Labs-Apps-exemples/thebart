3528acf8d04bb2dc584f8f5b21e0480702e4da0a
Nelson Reitz <nailsonne@gmail.com>
2015-03-16 16:38:56 +0100

    HTML/CSS Skeleton

--- views/index.php
+++ views/index.php
@@ -8,6 +8,7 @@
 
     <title>The BART</title>
 
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
 
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
@@ -16,18 +17,30 @@
 
   </head>
   <body>
-    <h1>The BART</h1>
-    <form id="route_form">
-      <select id="route_select">
+    <header class="header" role="banner">
 
-        <?php foreach ($routes as $route): ?>
-            <option value="<?= $route['number'] ?>"><?= $route['number'] ?> - <?= $route['name'] ?></option>
-        <?php endforeach ?>
+      <h1 class="site-title"><a href="/">The BART</a></h1>
+      <form id="route_form">
+        <select id="route_select">
 
-      </select>
-      <input type="submit">
-    </form>
-    <div id="map-canvas"></div>
-    <div id="php"></div>
+          <?php foreach ($routes as $route): ?>
+              <option value="<?= $route['number'] ?>"><?= $route['number'] ?> - <?= $route['name'] ?></option>
+          <?php endforeach ?>
+
+        </select>
+        <input type="submit" value="Submit">
+      </form>
+
+    </header>
+
+    <div id="map-canvas">
+    </div><!-- #map-canvas -->
+
+    <footer class="footer">
+      <p><a href="http://www.bart.gov/schedules/developers/api">Powered by BART API</a></p>
+      <p>This is a fictive website built as an exercise for the <a href="http://cs75.tv">Computer Science E-75</a> course.</p>
+      <p>More info at <a href="https://github.com/nelsonreitz/project2">github.com/nelsonreitz/project2</a>.</p>
+      <p>&copy; Nelson Reitz - 2015</p>
+    </footer>
   </body>
 </html>
