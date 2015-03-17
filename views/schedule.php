2f1f725841ceb5bde2b0855eeb316ed9e4df10f3
Nelson Reitz <nailsonne@gmail.com>
2015-03-18 00:21:13 +0100

    InfoWindow CSS

--- views/schedule.php
+++ views/schedule.php
@@ -1,34 +1,37 @@
-<h2><?= $station->name ?></h2>
+<div class="infowindow">
+  <div class="infowindow-header">
+    <h2><?= $station->name ?></h2>
+    <p>Next departures</p>
+  </div><!-- .infowindow-header -->
 
-<?php if (empty($departures)): ?>
+  <?php if (empty($departures)): ?>
 
-    <p>No departures scheduled in the next hour.</p>
+      <p>No departures scheduled in the next minutes.</p>
 
-<?php else: ?>
+  <?php else: ?>
 
-    <p>Next departures:</p>
-    <table>
+      <table>
 
-      <thead>
-        <tr>
-          <th>Destination:</th>
-          <th>Time:</th>
-        </tr>
-      </thead>
+        <thead>
+          <tr>
+            <th class="destination">Destination</th>
+            <th class="minutes">Time</th>
+          </tr>
+        </thead>
+        <tbody>
 
-      <tbody>
+          <?php foreach ($departures as $departure): ?>
 
-        <?php foreach ($departures as $departure): ?>
+                <tr>
+                  <th class="destination"><?= $departure['destination'] ?></th>
+                  <th class="minutes"><?= $departure['minutes'] ?><?= ($departure['minutes'] == 'Leaving') ? '' : ' min' ?></th>
+                </tr>
 
-              <tr>
-                <th><?= $departure['destination'] ?></th>
-                <th><?= $departure['minutes'] ?><?= ($departure['minutes'] == 'Leaving') ? '' : ' min' ?></th>
-              </tr>
+          <?php endforeach ?>
 
-        <?php endforeach ?>
+        </tbody>
 
-      </tbody>
+      </table>
 
-    </table>
-
-<?php endif ?>
+  <?php endif ?>
+</div><!-- .infowindow -->
